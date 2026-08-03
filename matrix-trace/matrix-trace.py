ff9cd03efb24167900e7eaf8bb9f2b3021c5c3de
Abhishek Tiwari <abhishri73555@gmail.com>
2026-08-03 10:39:29 +0530

    Solve matrix-trace

--- matrix-trace/matrix-trace.py
+++ matrix-trace/matrix-trace.py
@@ -4,8 +4,11 @@ def matrix_trace(A):
     """
     Compute the trace of a square matrix (sum of diagonal elements).
     """
-    # Write code here
-    sum=0
-    for i in range(len(A)):
-        sum+=A[i][i]
-    return sum
+    A_arr = np.asarray(A)
+    
+    # Validation: Ensure the matrix is 2D and square
+    if A_arr.ndim != 2 or A_arr.shape[0] != A_arr.shape[1]:
+        return None
+        
+    # Return the sum of diagonal elements as a scalar
+    return float(np.trace(A_arr))
